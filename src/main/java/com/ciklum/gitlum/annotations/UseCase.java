9114b68e608aba29248163eeaf8bdbf516227ade
Ventsislav Stoevski <cordonahcodecraft@gmail.com>
2022-10-19 15:19:22 +0300

    addresses #6
    Signed-off-by: Ventsislav Stoevski <cordonahcodecraft@gmail.com>

--- src/main/java/com/ciklum/gitlum/annotations/UseCase.java
+++ src/main/java/com/ciklum/gitlum/annotations/UseCase.java
@@ -9,6 +9,12 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/** This custom annotation is used to inform the developer, that the class, decorated with {@link com.ciklum.gitlum.annotations.UseCase},
+ * represents an implementation of the <a href="https://blog.cleancoder.com/uncle-bob/2012/08/13/the-clean-architecture.html">use case design pattern</a>.
+ * The annotation is alias for the {@link org.springframework.stereotype.Component} stereotype annotation and
+ * should be used only on classes, which must be managed by the Spring Context.
+ * @author Ventsislav Stoevski
+ */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
