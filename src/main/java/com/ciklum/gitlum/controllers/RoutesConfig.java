a2f5384c92a2864d4d74ce5e9df06e84c35e1a33
Ventsislav Stoevski <cordonahcodecraft@gmail.com>
2022-10-21 20:04:44 +0300

    closes #20 reimplements Swagger for webflux
    Signed-off-by: Ventsislav Stoevski <cordonahcodecraft@gmail.com>

--- src/main/java/com/ciklum/gitlum/controllers/RoutesConfig.java
+++ src/main/java/com/ciklum/gitlum/controllers/RoutesConfig.java
@@ -1,15 +1,20 @@
 package com.ciklum.gitlum.controllers;
 
+import com.ciklum.gitlum.domain.model.dto.RepoDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springdoc.core.annotations.RouterOperation;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
-
-
-
 
 @Configuration
 public class RoutesConfig {
@@ -20,6 +25,23 @@ public class RoutesConfig {
 	private String getGitRepositoriesURI;
 
 	@Bean
+	@RouterOperation(
+			method = GET,
+			path = "/api/v1/git-repositories/get",
+			produces = {MediaType.APPLICATION_JSON_VALUE},
+			beanClass = GitRepositoriesHandler.class,
+			beanMethod = "getGitRepositories",
+			operation = @Operation(
+					operationId = "getGitRepositories",
+					responses = {
+							@ApiResponse(
+									responseCode = "200",
+									description = "Successful operation",
+									content = @Content(schema = @Schema(implementation = RepoDTO.class))
+							)
+					}
+			)
+	)
 	public RouterFunction<ServerResponse> getGitRepositories(final GitRepositoriesHandler gitRepositoriesHandler) {
 		return route()
 				.GET(baseURL + getGitRepositoriesURI, gitRepositoriesHandler::getGitRepositories)
--- src/main/java/com/ciklum/gitlum/domain/model/dto/BranchDTO.java
+++ src/main/java/com/ciklum/gitlum/domain/model/dto/BranchDTO.java
@@ -1,14 +1,4 @@
 package com.ciklum.gitlum.domain.model.dto;
 
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
-
-@ApiModel(value = "Data transfer object for presentation of a Github repo branch per the business requirements")
-public record BranchDTO(
-		@ApiModelProperty(value = "The name of the branch", required = true)
-		String branchName,
-		@ApiModelProperty(value = "The value of the last commit sha", required = true)
-		String lastCommitSha
-) {
-
+public record BranchDTO(String branchName, String lastCommitSha) {
 }
